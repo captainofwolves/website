58cda999baba6ded2b534b65062b59ba21fbf4d4
Kevin Carmody <kevin@whiteoctober.co.uk>
2012-06-14 18:49:59 +0100

    test

--- www/git_prc.php
+++ www/git_prc.php
@@ -5,7 +5,7 @@ if(in_array($_SERVER['REMOTE_ADDR'], array('207.97.227.253', '50.57.128.197', '1
 
     try
     {
-      $payload = json_decode($_SERVER['payload']);
+      $payload = json_decode($_REQUEST['payload']);
     }
     catch(Exception $e)
     {
